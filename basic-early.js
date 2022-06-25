cc4c02ac0b449a29154cf2fab1c9a9126cd6ec5f
siebio <pawel.siebieszuk@intel.com>
2022-06-25 15:15:57 +0200

    Update basic-early.js
    Adds loop to make script running continuously

--- basic-early.js
+++ basic-early.js
@@ -5,14 +5,16 @@ export async function main(ns) {
 	let moneyThresh = ns.getServerMaxMoney(target) * 0.75;
 	let securityThresh = ns.getServerMinSecurityLevel(target) + 3;
 
-	if (ns.getServerSecurityLevel(target) > securityThresh){
-		//ns.exec("weak-target.js")
-		await ns.weaken(target)
-	} else if(ns.getServerMaxMoney < moneyThresh){
-		//ns.exec("grow-target.js")
-		await ns.grow(target)
-	} else{
-		//ns.exec("hack-target.js")
-		await ns.hack(target)
+	while(true){
+		if (ns.getServerSecurityLevel(target) > securityThresh){
+			//ns.exec("weak-target.js")
+			await ns.weaken(target)
+		} else if(ns.getServerMaxMoney < moneyThresh){
+			//ns.exec("grow-target.js")
+			await ns.grow(target)
+		} else{
+			//ns.exec("hack-target.js")
+			await ns.hack(target)
+		}
 	}
 }
